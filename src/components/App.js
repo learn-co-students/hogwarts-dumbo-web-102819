832548b612fdde0e70ff199a0a8420983385480a
ozubergz <jmin499@gmail.com>
2020-01-10 09:16:36 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,58 @@ import '../App.css';
 import Nav from './Nav'
 import hogs from '../porkers_data';
 
+import Hog from './Hog';
+import FilterBtn from './FilterBtn';
+
 class App extends Component {
+  
+  state = {
+    hogs: hogs,
+    showOnlyGreased: false,
+  }
+  
+  createHog = () => {
+    if(this.state.showOnlyGreased) {
+      let filtered = this.state.hogs.filter(hog => hog.greased);
+      return filtered.map(hog => <Hog hog={hog} />);
+    }  else {
+      return this.state.hogs.map(hog => <Hog hog={hog} />);
+    }
+  }
+
+  handleToggleFilter = () => {
+    this.setState({
+      showOnlyGreased: !this.state.showOnlyGreased
+    });
+  }
+
+  handleSortName = () => {
+    let hogs = this.state.hogs.slice();
+    hogs.sort((a, b) => (a.name.toUpperCase() > b.name.toUpperCase() ? 1 : -1))
+    this.setState({ hogs });
+  }
+
+  handleSortWeight = () => {
+    let hogs = this.state.hogs.slice();
+    hogs.sort((a, b) => a.weight - b.weight);
+    this.setState({ hogs });
+  }
+
   render() {
     return (
       <div className="App">
           < Nav />
-
+          <div className="button-group">
+            <FilterBtn
+              showOnlyGreased={this.state.showOnlyGreased}
+              handleToggleFilter={this.handleToggleFilter}
+            />
+            <button onClick={this.handleSortName}>Sort Name </button>
+            <button onClick={this.handleSortWeight}>Sort Weight</button>
+          </div>
+          <div className="ui grid container">
+            {this.createHog()}
+          </div>
       </div>
     )
   }
